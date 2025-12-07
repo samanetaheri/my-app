2f59877d4078feb7a32f9fb036f75d6b17fb7453
Samane Taheri <samaneh.taheri@snapp.kitchen>
2025-12-07 18:58:34 +0330

    rm test

--- cypress/e2e/2-advanced-examples/prompt.cy.js
+++ cypress/e2e/2-advanced-examples/prompt.cy.js
@@ -2,11 +2,8 @@ describe('Campaign Management', () => {
   it('creates multiple campaigns with different discount percentages', () => {
     cy.prompt(
           [
-            `Visit google.com`,
-            // Using {{adminPassword}} prevents this sensitive value from being sent to AI
-            'Type Duck into the input field',
-            'Click Sign In',
-            'click to search',
+            `Visit http://localhost:3000`,
+            'Click Deploy Now',
           ],
         )
   })
